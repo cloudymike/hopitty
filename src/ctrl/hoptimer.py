52877f861491d8e3b526d08eeb18ee63c1a82742
cloudymike <mikael@ubuntu-Latitude-D430.(none)>
2012-11-05 23:19:35 -0800

    hoptimer.py: use of minute fractions in hop timer

--- src/ctrl/hoptimer.py
+++ src/ctrl/hoptimer.py
@@ -4,18 +4,18 @@ import genctrl
 class hoptimer(genctrl.genctrl):
 
     def __init__(self):
-        self.actual = 0
-        self.target = 0
+        self.actual = 0.0
+        self.target = 0.0
         self.active = False
-        self.absminutes = time.localtime(time.time()).tm_min
+        self.absminutes = time.localtime(time.time()).tm_sec/60.0
         self.unit = 'min'
         self.powerOn = 'False'
 
     def measure(self):
-        currmin = time.localtime(time.time()).tm_min
+        currmin = time.localtime(time.time()).tm_sec/60.0
         deltamin = currmin - self.absminutes
         if deltamin < 0:
-            deltamin = deltamin + 60
+            deltamin = deltamin + 60.0
         self.absminutes = currmin
         if self.target > 0:
             self.actual = self.actual + deltamin
