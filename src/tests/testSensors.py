0e3b5fedbf0e0705aed401093ea133cdca9a7996
cloudymike <mikael@ubuntu-Latitude-D430.(none)>
2012-11-23 18:58:52 -0800

    testSensors.py added to test sensors subclass

---
+++ src/tests/testSensors.py
@@ -0,0 +1,26 @@
+import appliances
+import ctrl
+
+def testGenSensor():
+    c1=appliances.genctrl()
+    assert isinstance(c1.sensor.getID(), str)
+    assert (isinstance(c1.sensor.getValue(), int) or isinstance(c1.sensor.getValue(), float))
+
+def testSensors():
+    clist = ctrl.controllerList()
+    clist.load()
+    for key, c1 in clist.items():
+        print "Checking sensor", key
+        assert isinstance(c1.sensor.getID(), str)
+        assert (isinstance(c1.sensor.getValue(), int) or isinstance(c1.sensor.getValue(), float))
+   
+    
+if __name__ == "__main__":
+    c1=appliances.genctrl()
+    print c1.sensor.getID()
+    testGenSensor()
+    testSensors()
+    if isinstance(c1.sensor.getID(), str):
+        print "ID OK"
+    else:
+        print "ID FAIL"
