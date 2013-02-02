5f5a53606fc17328f92759aab3ed4b9b34a9cc7b
cloudymike <mikael@ubuntu-Latitude-D430.(none)>
2013-02-01 21:13:44 -0800

    recipelist update

--- src/recipelist/mycloudrecies.py
+++ src/recipelist/mycloudrecies.py
@@ -8,9 +8,12 @@ To be used by other modules, including web server
 # Use defs from controller...
 
 import sys
+import pickle
 import xml.dom.minidom
 #import xml.etree.ElementTree as ET
 import types
+import memcache
+#@PydevCodeAnalysisIgnore
 
 
 def bsmxReadString(doc, tagName):
@@ -41,7 +44,42 @@ def bsmxReadWeightLb(doc, tagName):
 
 class recipeClass():
     def __init__(self, name):
+        # All pertinent data in the recipe listed below
         self.name = name
+        self.equipment = None
+        self.mashMethod = None
+        self.infusionTemp = None
+        self.mashTime = None
+        self.infuseVolNet = None
+        self.infuseVolTot = None
+        self.tunDeadSpace = None
+        self.grainAbsorption = None
+        self.preboilVol = None
+        self.spargeTemp = None
+        self.grainWeight = None
+
+        # Alternative Data store
+        self.recipeDict = {}
+        self.recipeDict['equipment'] = None
+
+    def checkPopulation(self):
+        for key, val in self.recipeDict.items():
+            assert val != None
+
+    def getValue(self, key, value):
+        if key in self.recipeDict:
+            self.recipeDict[key] = value
+        else:
+            print "Error: recipe key "
+            sys.exit(1)
+
+    def readBMXdoc(self, doc):
+        self.setEquipment(bsmxReadString(doc, "F_E_NAME"))
+        self.recipeDict['equipment'] = bsmxReadString(doc, "F_E_NAME")
+
+    def printRecipeInfo(self):
+        print "    ", self.getEquipment()
+        print "    ", self.recipeDict['equipment']
 
     def setEquipment(self, equipment):
         self.equipment = equipment
@@ -53,8 +91,9 @@ class recipeClass():
         return self.name
 
 
+#===========================================================
 class recipeListClass():
-    def __init__(self):
+    def __init__(self, key=None):
         self.list = {}
 
     def newRecipe(self, name):
@@ -63,7 +102,9 @@ class recipeListClass():
 
     def storeToMemcache(self):
         """ Stores the class data into memcache record """
-        pass
+        rPickle = self.pickleMe()
+        mc = memcache.Client(['127.0.0.1:11211'], debug=0)
+        mc.set("recipeList", rPickle)
 
     def loadFromMemcache(self):
         """ Loads all data from memcache record """
@@ -74,18 +115,48 @@ class recipeListClass():
         for key, recipe in self.list.items():
             print key
 
-    def readBeerSmith(self, fileName):
+    def printAll(self):
+        """ Writes a list of all the recipe names"""
+        for key, recipe in self.list.items():
+            print key
+            recipe.printRecipeInfo()
+
+    def readBMXdoc(self, doc):
+        cloudRecipes = doc.getElementsByTagName("Cloud")
+        for recipe in cloudRecipes:
+            name = bsmxReadString(recipe, "F_R_NAME")
+            r = self.newRecipe(name)
+            r.readBMXdoc(recipe)
+            #r.setEquipment(bsmxReadString(recipe, "F_E_NAME"))
+
+    def readBeerSmithFile(self, fileName):
         bsmxFD = open(fileName)
         bsmxRawData = bsmxFD.read()
         bsmxFD.close()
 
         bsmxCleanData = bsmxRawData.replace('&', 'AMP')
         doc = xml.dom.minidom.parseString(bsmxCleanData)
-        cloudRecipes = doc.getElementsByTagName("Cloud")
-        for recipe in cloudRecipes:
-            name = bsmxReadString(recipe, "F_R_NAME")
-            r = self.newRecipe(name)
-            r.setEquipment(bsmxReadString(recipe, "F_E_NAME"))
+        return(doc)
+
+    def readBeerSmith(self, fileName):
+        doc = self.readBeerSmithFile(fileName)
+        self.readBMXdoc(doc)
+
+    def pickleMe(self):
+        rPickle = pickle.dumps(self)
+        return(rPickle)
+
+    def unPickleMe(self, rPickle):
+        rPickle = pickle.dumps(self)
+        return(rPickle)
+
+
+def objectFromMemcache(key):
+    mc = memcache.Client(['127.0.0.1:11211'], debug=0)
+
+    pickledObject = mc.get(key)
+    object = pickle.loads(pickledObject)
+    return(object)
 
 
 #======================== Nose test defs =============================
@@ -105,6 +176,11 @@ def getTestRecipeList():
 
 
 def testReading():
+    """ Do a check that recipe list read in is a valid set of recipes
+        Be thorough, as all other check are compared against the original
+        list.
+    """
+
     rl = getTestRecipeList()
     assert len(rl.list) > 0
     for name, recipe in rl.list.items():
@@ -115,9 +191,29 @@ def testReading():
         # Checks on each recipe
         assert isinstance(recipe.getEquipment(), types.StringTypes)
         assert len(recipe.getEquipment()) > 0
+        recipe.checkPopulation()
 
     #rl.printNameList()
     print "testReading passed"
+
+
+def testPickle():
+    rOrg = getTestRecipeList()
+    rPickle = rOrg.pickleMe()
+    rNew = pickle.loads(rPickle)
+    listEquivalence(rOrg, rNew)
+    #rNew.printNameList()
+    print "testPickle passed"
+
+
+def testMemcache():
+    rOrg = getTestRecipeList()
+    rOrg.storeToMemcache()
+    rNew = objectFromMemcache("recipeList")
+    #rNew = recipeListClass("recipeList")
+    listEquivalence(rOrg, rNew)
+    #rNew.printNameList()
+    print "testMemcache passed"
 
 
 def testEqualCheck():
@@ -134,9 +230,11 @@ if __name__ == "__main__":
     """
     testReading()
     testEqualCheck()
+    testPickle()
+    testMemcache()
 
     print "Alive"
     rl = recipeListClass()
     rl.readBeerSmith('/home/mikael/.beersmith2/Cloud.bsmx')
     print "==================Write'm out====================="
-    rl.printNameList()
+    rl.printAll()
