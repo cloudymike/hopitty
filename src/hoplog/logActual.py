e307c8f5031dd54eb9d1b20a72c5c62aba7b924c
cloudymike <mikael@cloudyforecast.com>
2013-09-29 19:57:01 -0700

    logActual initial working

--- src/hoplog/logActual.py
+++ src/hoplog/logActual.py
@@ -2,9 +2,13 @@
 Created on Sep 23, 2013
 
 @author: mikael
+
+This is probably not a logger but rather a plain file writer.
 '''
 import dataMemcache
 import logging
+import time
+import datetime
 
 
 class logActual():
@@ -12,42 +16,58 @@ class logActual():
     def __init__(self):
         self.data = dataMemcache.brewData()
         self.ctrls = self.data.getControllerList()
+        self.oldstatus = ''
+        self.oldControllers = None
+        self.oldStage = None
+        self.oldCurrentStage = None
+        self.oldLineTime = datetime.datetime.now()
 
-        # create logger
+        # create logger, may not be used
         self.logger = logging.getLogger(__name__)
         self.logger.setLevel(logging.INFO)
-
-        # create console handler and set level to debug
         ch = logging.StreamHandler()
         ch.setLevel(logging.DEBUG)
-
-        # create formatter
-        # formatter = logging.Formatter('%(asctime)s - %(name)s -
-        #                                %(levelname)s - %(message)s')
         formatter = logging.Formatter('%(asctime)s %(message)s')
-
-        # add formatter to ch
         ch.setFormatter(formatter)
-
-        # add ch to logger
         self.logger.addHandler(ch)
+
+    def outLine(self, message):
+        # self.logger.info(message)
+        now = datetime.datetime.now()
+        print str(now) + message
 
     def headLine(self):
         message = ""
         for c in self.ctrls:
             message = message + ", " + c
-        self.logger.info(message)
+        self.outLine(message)
 
     def logLine(self):
-        message = ""
         status = self.data.getStatus()
+        currentStage = self.data.getCurrentStage()
         controllers = status['controllers']
+        elapsed = datetime.datetime.now() - self.oldLineTime
+        if controllers != self.oldControllers:
+            if currentStage != self.oldCurrentStage or \
+               elapsed > datetime.timedelta(seconds=10):
+                self.oldLineTime = datetime.datetime.now()
+                message = ""
+                for c in self.ctrls:
+                    message = message + ", " + str(controllers[c]['actual'])
+                self.outLine(message)
+        self.oldControllers = controllers
+        self.oldCurrentStage = currentStage
 
-        for c in self.ctrls:
-            message = message + ", " + str(controllers[c]['actual'])
-        self.logger.info(message)
+    def oneLine(self):
+        runstatus = self.data.getRunStatus()
+        if runstatus == 'run' and runstatus != self.oldstatus:
+            self.headLine()
+        else:
+            self.logLine()
+        self.oldstatus = runstatus
 
 if __name__ == "__main__":
     l = logActual()
-    l.headLine()
-    l.logLine()
+    while (1):
+        l.oneLine()
+        time.sleep(1)
