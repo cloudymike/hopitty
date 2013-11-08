7a5e359ea1ff6a2b76178d8d286c6c3a384ee29e
cloudymike <mikael@cloudyforecast.com>
2013-11-08 07:18:46 -0800

    dymoScaleSensor, passing initial tests

--- src/sensors/dymoScaleSensor.py
+++ src/sensors/dymoScaleSensor.py
@@ -13,12 +13,10 @@ VENDOR_ID = 0x0922
 PRODUCT_ID = 0x8007
 
 
-
-
 class dymoScaleSensor(sensors.genericSensor):
 
     def __init__(self):
-        self.simval = 0
+        self.val = 0
         # find the USB device
         self.dev = usb.core.find(idVendor=VENDOR_ID,
               idProduct=PRODUCT_ID)
@@ -29,61 +27,90 @@ class dymoScaleSensor(sensors.genericSensor):
             self.simulation = False
             devmanufacturer = usb.util.get_string(self.dev, 256, 1)
             devname = usb.util.get_string(self.dev, 256, 2)
-            print "device found: " + devmanufacturer + " " + devname
+#            print "device found: " + devmanufacturer + " " + devname
+#
+#            interface = 0
+#            if self.dev.is_kernel_driver_active(interface) is True:
+#                print "but we need to detach kernel driver"
+#                self.dev.detach_kernel_driver(interface)
+#
+#                # use the first/default configuration
+#                self.dev.set_configuration()
+#                print "claiming device"
+#                usb.util.claim_interface(self.dev, interface)
 
-            interface = 0
-            if self.dev.is_kernel_driver_active(interface) is True:
-                print "but we need to detach kernel driver"
-                self.dev.detach_kernel_driver(interface)
+    def getID(self):
+        return(self.id)
 
-                # use the first/default configuration
-                self.dev.set_configuration()
-                print "claiming device"
-                usb.util.claim_interface(self.dev, interface)
+    def setID(self, newID):
+        self.id = newID
+
+    def readVol(self):
+        dev = usb.core.find(idVendor=VENDOR_ID,
+                               idProduct=PRODUCT_ID)
+        interface = 0
+        if dev.is_kernel_driver_active(interface) is True:
+            dev.detach_kernel_driver(interface)
+            # use the first/default configuration
+            dev.set_configuration()
+            usb.util.claim_interface(dev, interface)
+        else:
+            dev.set_configuration()
+
+        qt = None
+        for count in range(0, 10):
+            # first endpoint
+            endpoint = dev[0][(0, 0)][0]
+            try:
+                myd = dev.read(endpoint.bEndpointAddress,
+                               endpoint.wMaxPacketSize)
+            except:
+                myd = None
+            if myd != None:
+                raw_weight = myd[4] + myd[5] * 256
+
+                DATA_MODE_GRAMS = 2
+                DATA_MODE_KILOGRAMS = 3
+                DATA_MODE_OUNCES = 11
+                DATA_MODE_LB = 12
+
+                if myd[2] == DATA_MODE_OUNCES:
+                    ounces = raw_weight * 0.1
+                    grams = 28.3495 * ounces
+                elif myd[2] == DATA_MODE_GRAMS:
+                    grams = raw_weight
+                elif myd[2] == DATA_MODE_KILOGRAMS:
+                    grams = raw_weight * 100
+                elif myd[2] == DATA_MODE_LB:
+                    lbs = raw_weight / 10.0
+                    grams = 453.592 * lbs
+
+                qt = grams / 946.0
+                break
+
+        usb.util.release_interface(dev, interface)
+        dev.attach_kernel_driver(interface)
+        return(qt)
 
     def getValue(self):
+        if not self.simulation:
+            self.val = self.readVol()
+        return self.val
+
+    def setValue(self, val):
         if self.simulation:
-            return self.simval
+            self.val = val
+
+    def HWOK(self):
+        if self.simulation:
+            return(False)
         else:
-            data = self.grab()
-            if data != None:
-                raw_weight = data[4] + data[5] * 256
-                print raw_weight
-                if data[2] == DATA_MODE_OUNCES:
-                    ounces = raw_weight * 0.1
-                    weight = math.ceil(ounces)
-                    print_weight = "%s oz" % ounces
-                elif data[2] == DATA_MODE_GRAMS:
-                    grams = raw_weight
-                    weight = math.ceil(grams)
-                    print_weight = "%s g" % grams
-                return raw_weight
-
-
-    def grab(self):
-        try:
-            # first endpoint
-            endpoint = self.dev[0][(0, 0)][0]
-
-            # read a data packet
-            attempts = 10
-            data = None
-            while data is None and attempts > 0:
-                try:
-                    data = self.dev.read(endpoint.bEndpointAddress,
-                                       endpoint.wMaxPacketSize)
-                except usb.core.USBError as e:
-                    data = None
-                    if e.args == ('Operation timed out',):
-                        attempts -= 1
-                        print "timed out... trying again"
-                        continue
-
-            return data
-        except usb.core.USBError as e:
-            print "USBError: " + str(e.args)
-        except IndexError as e:
-            print "IndexError: " + str(e.args)
+            self.dev = usb.core.find(idVendor=VENDOR_ID,
+              idProduct=PRODUCT_ID)
+                # was it found?
+            if self.dev is None:
+                return(False)
+        return(True)
 
 
 if __name__ == '__main__':
