28a00b314c9a9009df0e95e878be634174608ae7
Mikael Hakansson <mikael@cloudyforecast.com>
2014-02-05 19:25:37 -0800

    delinted

--- src/bottleweb/mybottle.py
+++ src/bottleweb/mybottle.py
@@ -1,19 +1,23 @@
-from bottle import route, run, template, error, get, post, request, response, redirect
+from bottle import route, run, template, error, get, post
+from bottle import request, response, redirect
 import bottle
 import time
 
-counter = 1;
+counter = 1
+
 
 def update_counter():
     global counter
     counter = counter + 1
+
 
 @route('/')
 @route('/hello/<name>')
 def index(name='world'):
 
     if request.get_cookie("hello_visited"):
-        return template("Welcome back {{name}}</b>! Nice to see you again", name=name)
+        return template("Welcome back {{name}}</b>! Nice to see you again",\
+                name=name)
     else:
         response.set_cookie("hello_visited", "yes", max_age=60)
         return template('<b>Hello {{name}}</b>! Nice to meet you', name=name)
@@ -21,22 +25,24 @@ def index(name='world'):
 
 @route('/pretty')
 def pretty():
-    dapage='''
+    dapage = '''
     <h1>Big head</h1>
     hi
     '''
     return(dapage)
 
+
 @route('/refresh')
 def pretty():
 #    dapage=template('<b>Count is {{counter}}</b>', counter=counter)
-    dapage = """ 
+    dapage = """
     <meta http-equiv="refresh" content="5">
-    %i 
-    """ %counter;
+    %i
+    """ % counter
     update_counter()
 #    time.sleep(1)
     return(dapage)
+
 
 @get('/me')
 def me():
@@ -47,21 +53,25 @@ def me():
         </form>
     '''
 
-@post('/me') # or @route('/login', method='POST')
+
+@post('/me')  # or @route('/login', method='POST')
 def do_me_in():
     username = request.forms.get('username')
-    if username=='mikael':
+    if username == 'mikael':
         return "<p>Your username is correct.</p>"
     else:
         return "<p>That's not a good one.</p>"
+
 
 @error(404)
 def error404(error):
     return 'These are not the droids you are looking for'
 
+
 @route('/wrong')
 def wrong():
     redirect("/")
+
 
 bottle.debug(True)
 run(host='0.0.0.0', port=8080)
