d0697021c826dfdafb110a982dcc61e2f7af167d
Mikael Hakansson <mikael@cloudyforecast.com>
2014-05-23 07:33:43 -0700

    removed bottle-selenium

--- src/bottle-selenium/hopmain.py
+++
@@ -1,8 +0,0 @@
-from bottle import run, route, request, template
-import sys
-sys.path.append("../")
-from bottleweb import index
-
-
-if __name__ == '__main__':
-    run(reloader=True)
--- src/bottle-selenium/run_tests.old
+++
@@ -1,6 +0,0 @@
-import unittest
-
-import tests
-
-suite = unittest.TestLoader().loadTestsFromModule(tests)
-unittest.TextTestRunner(verbosity=2).run(suite)
