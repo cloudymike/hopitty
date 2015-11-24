741d055ed8f8024c3aacaf2578be5f980c2a34e9
Mikael Hakansson <mikael@cloudyforecast.com>
2015-11-24 08:05:30 -0800

    cleanup

--- src/switches/powerSwitch.py
+++ src/switches/powerSwitch.py
@@ -25,7 +25,7 @@ commands = {
 
 def send_command(cmd, read_response=False):
     ser = serial.Serial('/dev/ttyACM0', 9600)
-    ser.write(chr(cmd)+'\n')
+    ser.write(chr(cmd) + '\n')
     response = read_response and ser.read() or None
     ser.close()
     return response
