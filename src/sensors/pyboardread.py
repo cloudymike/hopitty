c962e132fbe01eba3276631c9f881c4508347cdd
Mikael Hakansson <mikael@cloudyforecast.com>
2016-07-24 16:33:17 -0700

    Added alt name for pyboard

--- src/sensors/pyboardread.py
+++ src/sensors/pyboardread.py
@@ -9,6 +9,8 @@ import pprint
 
 portName = \
     '/dev/serial/by-id/pci-Micro_Python_Pyboard_Virtual_Comm_Port_in_FS_Mode_000000000011-if01'
+altName = \
+    '/dev/serial/by-id/usb-Micro_Python_Pyboard_Virtual_Comm_Port_in_FS_Mode_000000000011-if01'
 baud = 115200
 
 class pyboardread():  # pragma: no cover
@@ -47,9 +49,21 @@ class pyboardread():  # pragma: no cover
                                         xonxoff=False,
                                         rtscts=False,
                                         dsrdtr=False)
-        except serial.serialutil.SerialException:
-            print("Unable to open port '%s'\r" % portName)
-            sys.exit(1)
+        except:
+            try:
+                
+                serPort = serial.Serial(port=altName,
+                                        baudrate=baud,
+                                        timeout=0.001,
+                                        bytesize=serial.EIGHTBITS,
+                                        parity=serial.PARITY_NONE,
+                                        stopbits=serial.STOPBITS_ONE,
+                                        xonxoff=False,
+                                        rtscts=False,
+                                        dsrdtr=False)
+            except serial.serialutil.SerialException:
+                print("Unable to open port '%s'\r" % portName)
+                sys.exit(1)
         
         time.sleep(0.02)
         
