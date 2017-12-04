c0354e043b31beab188b6e97752865601b0bb851
cloudymike <mikael@cloudyforecast.com>
2017-12-04 11:49:17 +0000

    Broke out functions for temp for reuse in js

--- src/webctrl/js/fetch.js
+++ src/webctrl/js/fetch.js
@@ -3,12 +3,13 @@ var allDisplays = document.querySelectorAll('pre');
 var tempDisplay = allDisplays[0]
 var myVar = setInterval(tempHWT, 1000);
 
-function tempHWT() {
-    
+function tempHWT() {tempAppliance('apitest', 'hwt'); }
+
+function tempAppliance(url, key) {
     fetch(url).then(function(response){
         response.json().then(function(json) {
             tempJson = json
-            tempHWT = tempJson['hwt']
+            tempHWT = tempJson[key]
             console.log(tempHWT)
             document.getElementById("tempHWT").innerHTML = tempHWT;
         });
