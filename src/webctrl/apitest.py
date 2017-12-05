e3a5d81b29bc7393fe7adc31a3ed23d8cfd176e6
cloudymike <mikael@cloudyforecast.com>
2017-12-05 21:32:24 +0000

    Variable refresh rate based on who is active

--- src/webctrl/apitest.py
+++ src/webctrl/apitest.py
@@ -2,14 +2,17 @@ import json
 import random
 
 def apipath(appliance):
-    print('apipath: ' + appliance)
+    print('appliance: ', appliance)
+
     if appliance == 'boiler':
         actual = random.randint(170,212)
         target = 205
+        active = False
     else:
         actual = random.randint(70,170)
         target = 168
-    active = True
+        active = True
+
     targetMet = actual >= target
     powerOn = active and (not targetMet)
     tempJson = {"actual": actual,"target":target,"unit":"F","active":active,"targetMet":targetMet, "powerOn":powerOn}
--- src/webctrl/js/fetch.js
+++ src/webctrl/js/fetch.js
@@ -1,21 +1,44 @@
-var url = 'apitest'
-var allDisplays = document.querySelectorAll('pre');
-var tempDisplay = allDisplays[0]
-var myVar = setInterval(tempHWT, 1000);
-var myVar = setInterval(tempBoiler, 10000);
+// stub functions to test java script
 
-function tempHWT() {pathAppliance('apipath', 'hwt'); }
-function tempBoiler() {pathAppliance('apipath', 'boiler'); }
+function getvalhwt() {pathAppliance('hwt', 'actual'); }
+function getvalboiler() {pathAppliance('boiler', 'actual'); }
 
-function pathAppliance(url, key) {
-    fullUrl = url + '/' + key
-    console.log(fullUrl)
+function initialize() {
+    refreshTime('hwt', getvalhwt);
+    refreshTime('boiler', getvalboiler);
+    //var myVar = setInterval(tempHWT, hwtTime);
+    //var myVar = setInterval(getvalboiler, 10000);
+    
+}
+
+function refreshTime(appliance, myF) {
+    var fullUrl = 'apipath' + '/' + appliance;
     fetch(fullUrl).then(function(response){
         response.json().then(function(json) {
-            tempJson = json
-            tempVal = tempJson['actual']
-            console.log(tempVal)
-            document.getElementById(key).innerHTML = tempVal;
+            var tempJson = json;
+            var isActive = tempJson['active'];
+            console.log("Value: " + isActive);
+            if (isActive) {
+                setInterval(myF, 1000);
+            } else {
+                setInterval(myF, 10000);
+            }
         });
     });
 }
+
+function pathAppliance(appliance, key) {
+    var fullUrl = 'apipath' + '/' + appliance;
+    console.log(fullUrl);
+    console.log(appliance, key);
+    fetch(fullUrl).then(function(response){
+        response.json().then(function(json) {
+            var tempJson = json;
+            var tempVal = tempJson[key];
+            console.log("Value: " + tempVal);
+            document.getElementById(appliance).innerHTML = tempVal;
+        });
+    });
+}
+
+initialize();
