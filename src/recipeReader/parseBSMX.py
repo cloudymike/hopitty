5743d2f44c4f03e3d68b984d5f3583054d0e7034
cloudymike <hakansso@pacbell.net>
2019-03-22 22:24:19 -0700

    Multiple equipment.fix (#37)
    * initial try
    
    * Ooops, did too much cleanup
    
    * back to basics

--- src/recipeReader/parseBSMX.py
+++ src/recipeReader/parseBSMX.py
@@ -75,6 +75,9 @@ class bsmxStages():
         specific fields should be used.
         """
         return(self.ctrl)
+    
+    def getCtrlEquipmentName(self):
+        return(self.ctrl.getEquipmentName())
 
     def getStages(self):
         """
