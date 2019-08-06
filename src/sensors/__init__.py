726c628ef9b09d63c550cd90ea2f71ba344bf190
cloudymike <hakansso@pacbell.net>
2019-08-05 21:51:48 -0700

    removed file and update test (#48)

--- src/sensors/__init__.py
+++ src/sensors/__init__.py
@@ -1,7 +1,6 @@
 from genericSensor import *
 from mashScaleSensor import *
 from dymoScaleSensor import *
-from thermometer import *
 from temperSensor import *
 from envTempSensor import *
 from pyboardTempSensor import *
--- src/tests/HWSensors.py
+++ src/tests/HWSensors.py
@@ -9,7 +9,7 @@ def countFails(scale, count):
     zeros = 0
     exceptions = 0
 
-    drone1 = sensors.thermometer()
+    drone1 = sensors.genericSensor()
     drone2 = sensors.temperSensor()
     switch1 = switches.coolerSwitch()
     switch2 = switches.myX10('/dev/serial/by-id/usb-Prolific_Technology'
