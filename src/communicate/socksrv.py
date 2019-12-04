f9cf807b325085190ad6882d783ac3c34f906796
cloudymike <mikael@cloudyforecast.com>
2019-12-04 05:06:14 +0000

    add main

--- src/communicate/socksrv.py
+++ src/communicate/socksrv.py
@@ -32,28 +32,29 @@ def readSocket(s, conn, status):
 
     return(conn, data)
 
+if __name__ == "__main__":
  
-TCP_IP = '127.0.0.1'
-TCP_PORT = 10062
-BUFFER_SIZE = 20  # Normally 1024, but we want fast response
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.bind((TCP_IP, TCP_PORT))
-# Create a few sockets in case they are not quickly released
-s.listen(10)
-print 'Starting loop'
-s.setblocking(False)
-
-
-
-
-
-conn = None
-while 1:
-
-    conn, data = readSocket(s,conn,'All OK')
-    # This would be the program
-    print("Doing stuff with {}".format(data))
-    time.sleep(1)
-
-print 'Program ending'
-
+    TCP_IP = '127.0.0.1'
+    TCP_PORT = 10062
+    BUFFER_SIZE = 20  # Normally 1024, but we want fast response
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.bind((TCP_IP, TCP_PORT))
+    # Create a few sockets in case they are not quickly released
+    s.listen(10)
+    print 'Starting loop'
+    s.setblocking(False)
+    
+    
+    
+    
+    
+    conn = None
+    while 1:
+    
+        conn, data = readSocket(s,conn,'All OK')
+        # This would be the program
+        print("Doing stuff with {}".format(data))
+        time.sleep(1)
+    
+    print 'Program ending'
+    
