0964910fd7c8f512d7a6fb6df49c8436394a999c
cloudymike <mikael@cloudyforecast.com>
2020-01-26 06:03:27 +0000

    bsmx and brewersfriend version

---
+++ src/brewersfriend/run.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+python todict.py -f parfait.xml
+python todict.py -b ../../beersmith/93george.bsmx
--- src/brewersfriend/todict.py
+++ src/brewersfriend/todict.py
@@ -1,6 +1,23 @@
 import xmltodict
+import argparse
 
-with open('parfait.xml') as fd:
-    dict = xmltodict.parse(fd.read())
-    
-print(dict)
+parser = argparse.ArgumentParser()
+group = parser.add_mutually_exclusive_group(required=True)
+group.add_argument('-b', '--bsmx', default=None, help='Beersmith file to use, bsmx format, ')
+group.add_argument('-f', '--brewersfriend', default=None, help='Brewers friend file to use, xml format, ')
+
+args = parser.parse_args()
+
+if args.bsmx:
+    with open(args.bsmx) as fd:
+        dict = xmltodict.parse(fd.read())
+    recipename=dict['Recipes']['Data']['Recipe']['F_R_NAME']
+
+if args.brewersfriend:
+    with open(args.brewersfriend) as fd:
+        dict = xmltodict.parse(fd.read())
+    recipename=dict['RECIPES']['RECIPE']['NAME']
+
+
+#print(dict)
+print("Recipe name: {}".format(recipename))
