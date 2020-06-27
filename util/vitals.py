6b5bce213fdea1d7def0d5d121ec235c42cb1c66
cloudymike <mikael@cloudyforecast.com>
2020-06-27 04:52:20 +0000

    wip

--- util/vitals.py
+++ util/vitals.py
@@ -7,13 +7,28 @@ parser = argparse.ArgumentParser(description='Read a stages file and create a ta
 parser.add_argument('-f', '--file', required=True, help='Input JSON file')
 args = parser.parse_args()
 
+def reprintCsv(headers, theMatrix):
+    for col in header:
+        print('|{}'.format(col)),
+    print('|')
+    for row in theMatrix:
+        for col in row:
+            print('|{}'.format(col)),
+        print('|')
+
+def column(theMatrix, colindex):
+    col = []
+    for row in theMatrix:
+        col.append(row[colindex])
+    return(col)
+
 with open(args.file, 'rb') as csvfile:
-    spamreader = csv.reader(csvfile, delimiter=',', quotechar='"')
+    csvreader = csv.reader(csvfile, delimiter=',', quotechar='"')
     isHeader = True
-    header = []
     theMatrix = []
-    for row in spamreader:
+    for row in csvreader:
         if isHeader:
+            header = []
             isHeader = False
             for col in row:
                 header.append(col)
@@ -23,15 +38,13 @@ with open(args.file, 'rb') as csvfile:
                 aRow.append(col)
             theMatrix.append(aRow)
             
-for col in header:
-    print('|{}'.format(col)),
-print('|')
-for row in theMatrix:
-    for col in row:
-        print('|{}'.format(col)),
-    print('|')
-        
+
 print('Columns: {}'.format(len(header)))
 print('Rows: {}'.format(len(theMatrix)))
+print('Index of Time: {}'.format(header.index('Time')))
+print('Index of cooler: {}'.format(header.index('cooler')))
+print('Index of boilerVolume: {}'.format(header.index('boilerVolume')))
+print('Max boilerVolume: {}'.format(max(column(theMatrix,header.index('boilerVolume')))))
 
+reprintCsv(header, theMatrix)
 
