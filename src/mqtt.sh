47a18e0be132f07c4c3763644ece4890556447f9
cloudymike <mikael@cloudyforecast.com>
2020-08-04 20:24:58 -0700

    Wrapper for complete startup

---
+++ src/mqtt.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+# Runs the front end and backend for the mqtt based server
+# For testing
+
+# Starting mosquitto should not be needed but included for completeness
+sudo service mosquitto start
+
+
+export FN_AUTH_REDIRECT_URI=http://localhost:8080/google/auth
+export FN_BASE_URI=http://localhost:8080
+export FLASK_DEBUG=0
+export FN_FLASK_SECRET_KEY=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)
+
+pushd hopfront
+python ./web.py -m &
+popd
+
+# Start backend
+./runmqtt.py -m
+
+# kill webserver
+kill %1
