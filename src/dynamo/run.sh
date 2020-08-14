7308e2aeed5029298d3a6824a146f2f8a2b5d8dc
cloudymike <hakansso@pacbell.net>
2020-08-14 15:20:07 -0700

    Runnable script for eval (#114)
    Co-authored-by: cloudymike <mikael@cloudyforecast.com>

--- src/dynamo/run.sh
+++ src/dynamo/run.sh
@@ -1,7 +1,25 @@
 #!/bin/bash
 
+if [ ! -f ../out.json ]
+then
+   pushd ..
+   python dynamorecipes.py -d
+   popd
+fi
+
+
+
+if [ ! -f localdynamodb/DynamoDBLocal.jar ]
+then
+   mkdir -p localdynamodb
+   pushd localdynamodb
+   wget https://s3.us-west-2.amazonaws.com/dynamodb-local/dynamodb_local_latest.tar.gz
+   tar xvzf dynamodb_local_latest.tar.gz 
+   popd
+fi
+
 # Start local dynamodb
-java -Djava.library.path=~/dynamodb/DynamoDBLocal_lib -jar ~/dynamodb/DynamoDBLocal.jar -sharedDb &
+java -Djava.library.path=./localdynamodb/DynamoDBLocal_lib -jar ./localdynamodb/DynamoDBLocal.jar -sharedDb &
 
 # Check that dynamodb is working locally
 aws dynamodb list-tables --endpoint-url http://localhost:8000
