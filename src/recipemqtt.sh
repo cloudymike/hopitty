3956848d139419a8263e871b742c52162799c463
cloudymike <hakansso@pacbell.net>
2020-09-02 16:08:55 -0700

    testscript (#141)
    Co-authored-by: cloudymike <mikael@cloudyforecast.com>

--- src/recipemqtt.sh
+++ src/recipemqtt.sh
@@ -1,13 +1,24 @@
 #!/bin/bash
 # Script to read in a recipe, and then create a json stages and run runmqtt with it
 # Mainly for testing
-if [ "$1" == "" ]
-then
-  echo "USAGE: $0 recipefile"
-  exit 1
-fi
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+OPTIONS=''
+while getopts "f:qc" opt; do
+  case ${opt} in
+    f ) INFILE=$OPTARG
+      ;;
+    q ) OPTIONS=$OPTIONS' -q'
+      ;;
+    c ) OPTIONS=$OPTIONS' -c'
+      ;;
+  esac
+done
 
 TMPFILENAME='/tmp/'$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)
 
-./json2stages.py -i $1 -o $TMPFILENAME -e 'Grain 3G, 5Gcooler, 5Gpot, platechiller'
-./runmqtt.py -n -r $TMPFILENAME -t 'Grain 3G, 5Gcooler, 5Gpot, platechiller'
+$DIR/json2stages.py -i $INFILE -o $TMPFILENAME -e 'Grain 3G, 5Gcooler, 5Gpot, platechiller'
+$DIR/runmqtt.py -n -r $TMPFILENAME -t 'Grain 3G, 5Gcooler, 5Gpot, platechiller' $OPTIONS
+
+rm $TMPFILENAME
