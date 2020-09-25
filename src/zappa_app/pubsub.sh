d5db97f0c678cfb0683c73b78411029901f889eb
cloudymike <hakansso@pacbell.net>
2020-09-24 21:05:27 -0700

    Cleanup (#147)
    Co-authored-by: cloudymike <mikael@cloudyforecast.com>

--- src/zappa_app/pubsub.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-
-python pubsub.py \
-   --endpoint a2d09uxsvr5exq-ats.iot.us-east-1.amazonaws.com \
-   --root-ca /home/mikael/secrets/certs/awsrootca1.crt \
-   --cert /home/mikael/secrets/certs/e27d28a42b-certificate.pem.crt \
-   --key /home/mikael/secrets/keys/e27d28a42b-private.pem.key
