f129cdf27ef6d085a0bf59e8313b2b2b10710124
cloudymike <mikael@cloudyforecast.com>
2024-10-18 08:47:45 -0700

    Working

--- src/onlinebeersmith/prototype.sh
+++ src/onlinebeersmith/prototype.sh
@@ -1,26 +1,34 @@
 #!/bin/bash
 
+# Remember to use venv from top level....
+# source ../../venv/bin/activate
+
 USER=5399
 TYPEOFEQUIPMENT="Grain 3G, 5Gcooler, 5Gpot, mashexittemp"
 
-OUTPUTFILE=/tmp/test.bsmx
-rm -f $OUTPUTFILE
+JSONDIR=/tmp/tmpjsonrecipies
+
+rm -rf $JSONDIR
+mkdir $JSONDIR
+
 # Find all recipies that are public by USER
 # Should really loop until empty. Each batch is 20 recipies
-
 LIST0=$(curl -s https://beersmithrecipes.com/listrecipes/5399/0 | grep viewrecipe | cut -d/ -f5) 
 LIST1=$(curl -s https://beersmithrecipes.com/listrecipes/5399/1 | grep viewrecipe | cut -d/ -f5) 
 
 LIST="$LIST0 $LIST1"
-for RECIPEID in $LIST1 
+for RECIPEID in $LIST 
 do
 	echo $RECIPEID
-	wget -qO - https://beersmithrecipes.com/download.php?id=$RECIPEID >> $OUTPUTFILE
+	rm -f /tmp/tmp.bsmx
+	wget -qO - https://beersmithrecipes.com/download.php?id=$RECIPEID > /tmp/tmp.bsmx
+
+	pushd ..
+	python2 dynamorecipes.py -b /tmp/tmp.bsmx -f $JSONDIR/$RECIPEID.json -t "$TYPEOFEQUIPMENT"
+	popd
 
 done
 
-# Remember to use venv from top level....
-# source ../../venv/bin/activate
-pushd ..
-python2 dynamorecipes.py -b /tmp/test.bsmx -t "$TYPEOFEQUIPMENT"
-popd
+cat $JSONDIR/*.json | jq -s 'add' > ../out.json
+echo "====================================== Current output file ========================================"
+jq .[].recipe_name ../out.json
