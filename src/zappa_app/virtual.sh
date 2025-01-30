3b4c9b0abae17ba7490e4f86966adc974455e0b2
cloudymike <mikael@cloudyforecast.com>
2025-01-30 19:00:08 +0100

    One front end (#201)
    * zappa
    
    * s3upload
    
    * flask
    
    * docker_app
    
    * splitupload

--- src/zappa_app/virtual.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-if [ "$VIRTUAL_ENV" == "" ]
-then
-  virtualenv -p python3 venv
-  source venv/bin/activate
-  pip install -r requirements.txt
-else
-  echo Virtual env already installed in $VIRTUAL_ENV
-fi
